cf1017f93428eee9622ce6b86e940479cb1bc0be
xbynet <xbynet@outlook.com>
2017-07-25 10:29:52 +0800

    1

--- src/main/java/com/github/xbynet/config/CommonConfig.java
+++ src/main/java/com/github/xbynet/config/CommonConfig.java
@@ -4,10 +4,12 @@ import javax.persistence.EntityManagerFactory;
 
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.support.OpenSessionInViewFilter;
 import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -30,5 +32,13 @@ public class CommonConfig {
 	public PlatformTransactionManager transactionManager(SessionFactory sessionFactory) {
 		return new HibernateTransactionManager(sessionFactory);
 	}
-
+	//https://stackoverflow.com/questions/36979281/using-spring-boot-and-data-jpa-getting-lazyinitializationexception-despite-the
+	@Bean
+    public FilterRegistrationBean registerOpenSessionInViewFilterBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        OpenSessionInViewFilter filter = new OpenSessionInViewFilter();
+        registrationBean.setFilter(filter);
+        registrationBean.setOrder(10);
+        return registrationBean;
+    }
 }
